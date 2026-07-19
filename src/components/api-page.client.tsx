90369654b784816c35bda7ea29b08159dbfa0ad9
Jyotirmoy Bandyopadhayaya <jyotirmoy.bandyopadhayaya@loginradius.com>
2026-07-19 13:54:51 +0530

    chore: remove unused api-page.client.tsx

--- src/components/api-page.client.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import { defineClientConfig } from 'fumadocs-openapi/ui/client';
-
-export default defineClientConfig({
-  playground: {
-    transformAuthInputs: (inputs) => inputs,
-  },
-});
